51de5497dcf6770967ae8da5c648413ff938e9dd
hung2xt <46932460+hung2xt@users.noreply.github.com>
2023-11-23 16:18:13 +0000

    Second commit

---
+++ download-mysql-publish-data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+RUN wget https://www.mysqltutorial.org/wp-content/uploads/2023/10/mysqlsampledatabase.zip && \
+    unzip mysqlsampledatabase.zip && \
+    rm mysqlsampledatabase.zip
+
+    
